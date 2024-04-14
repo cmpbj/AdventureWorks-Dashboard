66dd474d53e9b662117f191aee2a04f98f50ac22
cmpbj <magnocarlos3232@gmail.com>
2024-04-14 01:40:11 -0300

    Corrige bug

--- dashboard/app.py
+++ dashboard/app.py
@@ -67,7 +67,7 @@ with st.expander('Dataset info'):
 col1, col2, col3, col4 = st.columns(4)
 
 df_temp_cards = df.set_index(['Order Date'])
-df = df.sort_index()
+df_temp_cards = df_temp_cards.sort_index()
 
 
 with col1:
